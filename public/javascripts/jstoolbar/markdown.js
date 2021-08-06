ea58b46c949dec1bbf2a271b5cb93163f085af58
Karol Hosiawa <hosiawak@gmail.com>
2021-08-06 09:16:22 +0200

    Copied public assets

--- public/javascripts/jstoolbar/markdown.js
+++ public/javascripts/jstoolbar/markdown.js
@@ -26,6 +26,7 @@
 jsToolBar.prototype.elements.strong = {
   type: 'button',
   title: 'Strong',
+  shortcut: 'b',
   fn: {
     wiki: function() { this.singleTag('**') }
   }
@@ -35,8 +36,19 @@ jsToolBar.prototype.elements.strong = {
 jsToolBar.prototype.elements.em = {
   type: 'button',
   title: 'Italic',
+  shortcut: 'i',
   fn: {
     wiki: function() { this.singleTag("*") }
+  }
+}
+
+// ins
+jsToolBar.prototype.elements.ins = {
+  type: 'button',
+  title: 'Underline',
+  shortcut: 'u',
+  fn: {
+    wiki: function() { this.singleTag('_') }
   }
 }
 
@@ -161,12 +173,30 @@ jsToolBar.prototype.elements.unbq = {
   }
 }
 
+// table
+jsToolBar.prototype.elements.table = {
+  type: 'button',
+  title: 'Table',
+  fn: {
+    wiki: function() {
+      var This = this;
+      this.tableMenu(function(cols, rowCount){
+        This.encloseLineSelection(
+          '|'+cols.join(' |')+' |\n' +                                   // header
+          Array(cols.length+1).join('|--')+'|\n' +                       // second line
+          Array(rowCount+1).join(Array(cols.length+1).join('|  ')+'|\n') // cells
+        );
+      });
+    }
+  }
+}
+
 // pre
 jsToolBar.prototype.elements.pre = {
   type: 'button',
   title: 'Preformatted text',
   fn: {
-    wiki: function() { this.encloseLineSelection('~~~\n', '\n~~~') }
+    wiki: function() { this.encloseLineSelection('```\n', '\n```') }
   }
 }
 
@@ -178,7 +208,7 @@ jsToolBar.prototype.elements.precode = {
     wiki: function() {
       var This = this;
       this.precodeMenu(function(lang){
-        This.encloseLineSelection('~~~ ' + lang + '\n', '\n~~~\n');
+        This.encloseLineSelection('``` ' + lang + '\n', '\n```\n');
       });
     }
   }
